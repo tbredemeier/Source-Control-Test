2b870cedd158f50f9eb6e80074cc8bc6b5276fe5
Tom Bredemeier <tbredemeier@barrington220.org>
2022-07-21 14:21:19 -0500

    Initial Commit

---
+++ Source Control Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Source Control Test
+//
+//  Created by Tom Bredemeier on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source Control Test/Source_Control_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_Control_TestApp.swift
+//  Source Control Test
+//
+//  Created by Tom Bredemeier on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Source_Control_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
